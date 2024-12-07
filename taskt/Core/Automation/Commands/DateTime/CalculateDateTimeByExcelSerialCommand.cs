d94f7a9f15a21bc2b5e745c111ab7d618ee49118
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2024-12-07 17:46:58 +0900

    add commands

---
+++ taskt/Core/Automation/Commands/DateTime/CalculateDateTimeByExcelSerialCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+using taskt.Core.Script;
+
+namespace taskt.Core.Automation.Commands
+{
+    [Serializable]
+    [Attributes.ClassAttributes.Group("DateTime")]
+    [Attributes.ClassAttributes.SubGruop("")]
+    [Attributes.ClassAttributes.CommandSettings("Calculate DateTime By Excel Serial")]
+    [Attributes.ClassAttributes.Description("This command allows you to Calculate DateTime By Excel Serial. Add Day, Minute, etc.")]
+    [Attributes.ClassAttributes.UsesDescription("Use this command when you want to Calculate DateTime By Excel Serial. Add Day, Minute, etc.")]
+    [Attributes.ClassAttributes.ImplementationDescription("")]
+    [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_function))]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
+    public sealed class CalculateDateTimeByExcelSerialCommand : ACalculateDateTimeCommands
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(DateTimeControls), nameof(DateTimeControls.v_ExcelSerial))]
+        public override string v_DateTime { get; set; }
+
+        //[XmlAttribute]
+        //public string v_CalculationMethod { get; set; }
+
+        //[XmlAttribute]
+        //public string v_Value { get; set; }
+
+        //[XmlAttribute]
+        //public override string v_Result { get; set; }
+
+        public CalculateDateTimeByExcelSerialCommand()
+        {
+        }
+
+        public override void RunCommand(Engine.AutomationEngineInstance engine)
+        {
+            using (var myDT = new InnerScriptVariable(engine)) 
+            {
+                var cdc = new CreateDateTimeFromExcelSerialCommand()
+                {
+                    v_Serial = this.v_DateTime,
+                    v_DateTime = myDT.VariableName,
+                };
+                cdc.RunCommand(engine);
+
+                var calc = new CalculateDateTimeCommand()
+                {
+                    v_DateTime = myDT.VariableName,
+                    v_CalculationMethod = this.v_CalculationMethod,
+                    v_Value = this.v_Value,
+                    v_Result = this.v_Result,
+                };
+                calc.RunCommand(engine);
+            }
+        }
+    }
+}
---
+++ taskt/Core/Automation/Commands/DateTime/CalculateDateTimeByTextCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+using taskt.Core.Script;
+
+namespace taskt.Core.Automation.Commands
+{
+    [Serializable]
+    [Attributes.ClassAttributes.Group("DateTime")]
+    [Attributes.ClassAttributes.SubGruop("")]
+    [Attributes.ClassAttributes.CommandSettings("Calculate DateTime By Text")]
+    [Attributes.ClassAttributes.Description("This command allows you to Calculate DateTime By Text. Add Day, Minute, etc.")]
+    [Attributes.ClassAttributes.UsesDescription("Use this command when you want to Calculate DateTime By Text. Add Day, Minute, etc.")]
+    [Attributes.ClassAttributes.ImplementationDescription("")]
+    [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_function))]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
+    public sealed class CalculateDateTimeByTextCommand : ACalculateDateTimeCommands
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(DateTimeControls), nameof(DateTimeControls.v_DateTimeText))]
+        public override string v_DateTime { get; set; }
+
+        //[XmlAttribute]
+        //public string v_CalculationMethod { get; set; }
+
+        //[XmlAttribute]
+        //public string v_Value { get; set; }
+
+        //[XmlAttribute]
+        //public override string v_Result { get; set; }
+
+        public CalculateDateTimeByTextCommand()
+        {
+        }
+
+        public override void RunCommand(Engine.AutomationEngineInstance engine)
+        {
+            using (var myDT = new InnerScriptVariable(engine)) 
+            {
+                var cdt = new CreateDateTimeFromTextCommand()
+                {
+                    v_Text = this.v_DateTime,
+                    v_DateTime = myDT.VariableName,
+                };
+                cdt.RunCommand(engine);
+
+                var calc = new CalculateDateTimeCommand()
+                {
+                    v_DateTime = myDT.VariableName,
+                    v_CalculationMethod = this.v_CalculationMethod,
+                    v_Value = this.v_Value,
+                    v_Result = this.v_Result,
+                };
+                calc.RunCommand(engine);
+            }
+        }
+    }
+}
