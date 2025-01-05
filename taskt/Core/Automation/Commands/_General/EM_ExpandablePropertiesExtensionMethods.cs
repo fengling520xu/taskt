c7bb3806283ccb153753613f83a419e7306cf9a5
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-01-05 14:45:01 +0900

    add extension method

---
+++ taskt/Core/Automation/Commands/_General/EM_ExpandablePropertiesExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskt.Core.Automation.Commands._General
+{
+    /// <summary>
+    /// extension methods for ILExpandableProperties
+    /// </summary>
+    public static class EM_ExpandablePropertiesExtensionMethods
+    {
+        /// <summary>
+        /// convert to ScriptCommand
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        public static ScriptCommand ToScriptCommand(this ILExpandableProperties command)
+        {
+            // TODO: It will eventually go out of use.
+            return (ScriptCommand)command;
+        }
+    }
+}
