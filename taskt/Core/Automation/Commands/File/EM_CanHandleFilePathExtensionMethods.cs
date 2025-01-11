feea0887f0a30a60a3deff7c44a7a24d4125090a
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-01-11 21:16:46 +0900

    add interface, class

---
+++ taskt/Core/Automation/Commands/File/EM_CanHandleFilePathExtensionMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// Handle FilePath Extension methods
+    /// </summary>
+    public static class EM_CanHandleFilePathExtensionMethods
+    {
+        /// <summary>
+        /// expand value or User Variable As File name
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="engine"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static string ExpandValueOrUserVariableAsFilePath(this ICanHandleFilePath command, string parameterName, Engine.AutomationEngineInstance engine)
+        {
+            var prop = command.ToScriptCommand().GetProperty(parameterName);
+            string parameterValue = prop.GetValue(command)?.ToString() ?? "";
+
+            var fn = parameterValue.ExpandValueOrUserVariable(engine);
+            var invs = Path.GetInvalidFileNameChars();
+            if (fn.IndexOfAny(invs) < 0)
+            {
+                return fn;
+            }
+            else
+            {
+                throw new Exception($"File Name contains invalid chars. File Name: '{fn}'");
+            }
+        }
+    }
+}
---
+++ taskt/Core/Automation/Commands/File/ICanHandleFileName.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for handle file name properties
+    /// </summary>
+    public interface ICanHandleFileName : ILExpandableProperties
+    {
+        // nothing
+    }
+}
