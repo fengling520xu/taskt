c9a86504587d9d2201a899cc8ee34cf84b6849ca
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-01-12 20:50:14 +0900

    add descriptions

--- taskt/Core/Automation/Commands/File/AFileExistsFilePathBeforeAfterResultCommands.cs
+++ taskt/Core/Automation/Commands/File/AFileExistsFilePathBeforeAfterResultCommands.cs
@@ -10,11 +10,13 @@ namespace taskt.Core.Automation.Commands
     {
         [XmlAttribute]
         [PropertyVirtualProperty(nameof(FilePathControls), nameof(FilePathControls.v_FilePathResult))]
+        [PropertyDescription("Variable Name to Store File Path Before Command")]
         [PropertyParameterOrder(20000)]
         public virtual string v_BeforeFilePathResult { get; set; }
 
         [XmlAttribute]
         [PropertyVirtualProperty(nameof(FilePathControls), nameof(FilePathControls.v_FilePathResult))]
+        [PropertyDescription("Variable Name to Store File Path After Command")]
         [PropertyParameterOrder(21000)]
         public virtual string v_AfterFilePathResult { get; set; }
     }
