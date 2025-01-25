3a511b14dd61fe0ea5f7447655ff9c6598a044c6
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-01-25 23:13:05 +0900

    add abstract classes

---
+++ taskt/Core/Automation/Commands/Folder/AFolderExistsFolderPathCommands.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for exists folder commands
+    /// </summary>
+    public abstract class AFolderExistsFolderPathCommands : AFolderFolderPathCommands, IFolderExistsFolderPathProperties
+    {
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(FolderPathControls), nameof(FolderPathControls.v_WaitTime))]
+        [PropertyParameterOrder(10000)]
+        public virtual string v_WaitTimeForFolder { get; set; }
+    }
+}
---
+++ taskt/Core/Automation/Commands/Folder/AFolderExistsFolderPathPathResultCommands.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for exists folder and get folder path commands
+    /// </summary>
+    public abstract class AFolderExistsFolderPathPathResultCommands : AFolderExistsFolderPathCommands, IFolderExistsFolderPathPathResultProperties
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(FolderPathControls), nameof(FolderPathControls.v_FolderPathResult))]
+        [PropertyParameterOrder(20000)]
+        public virtual string v_ResultPath { get; set; }
+    }
+}
