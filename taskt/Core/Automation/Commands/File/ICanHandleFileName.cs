6dd0f4b3383c0b5a37eaaaaacc5e83a15630c5bc
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-02-02 21:00:16 +0900

    change summery

--- taskt/Core/Automation/Commands/File/ICanHandleFileName.cs
+++ taskt/Core/Automation/Commands/File/ICanHandleFileName.cs
@@ -1,7 +1,7 @@
 ﻿namespace taskt.Core.Automation.Commands
 {
     /// <summary>
-    /// for handle file name properties
+    /// for handle file name
     /// </summary>
     public interface ICanHandleFileName : ICanHandleFileOrFolderName
     {
