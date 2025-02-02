08b987fb4403e5aa4ca370e6cc63c9fb0571b5c5
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-02-02 15:32:32 +0900

    change imple interface

--- taskt/Core/Automation/Commands/File/ICanHandleFileName.cs
+++ taskt/Core/Automation/Commands/File/ICanHandleFileName.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// for handle file name properties
     /// </summary>
-    public interface ICanHandleFileName : ILExpandableProperties
+    public interface ICanHandleFileName : ICanHandleFileOrFolderName
     {
         // nothing
     }
