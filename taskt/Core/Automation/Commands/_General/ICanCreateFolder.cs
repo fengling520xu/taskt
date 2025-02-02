a9aff1db62960c057b50511d2adb270d9147e4a3
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-02-02 15:29:01 +0900

    add new interfaces

---
+++ taskt/Core/Automation/Commands/_General/ICanCreateFolder.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for create folder commands
+    /// </summary>
+    public interface ICanCreateFolder
+    {
+        // nothing
+    }
+}
